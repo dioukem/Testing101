0a36dc3d9178a7202c99cad3d2de0d3fae7720a2
dioukem <35600366+dioukem@users.noreply.github.com>
2019-02-03 02:09:54 -0500

    adding namespace
    Adding namespace and the process to console the program exiting

--- Hello.cs
+++ Hello.cs
@@ -1,3 +1,9 @@
-public static void Main(string[] args){
-  Console.WriteLine("Hello World");
+using System;
+namespace HelloWorld{
+
+      public static void Main(string[] args){
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Press enter to finish the program");
+        Console.Read();
+      }
 }
